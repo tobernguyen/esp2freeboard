bfdb9352b76af311e365c7a5ba4f3b196995ec84
Long Nguyen <tobernguyen@gmail.com>
2016-10-17 01:12:36 +0700

    Change default mqtt server configuration

--- libs/mqtt.js
+++ libs/mqtt.js
@@ -1,5 +1,5 @@
 var mqtt = require('mqtt');
-var client = mqtt.connect(process.env.MQTT_URI || 'mqtt://localhost:1883');
+var client = mqtt.connect(process.env.MQTT_URI || 'mqtt://127.0.0.1:1883');
 
 client.on('connect', () => {
   console.log('Connected to MQTT server. Ready to subscribe.');
