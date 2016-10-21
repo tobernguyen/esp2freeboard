10847c150f1213afa4a95920f4df79e485db1148
Long Nguyen <tobernguyen@gmail.com>
2016-10-22 02:27:35 +0700

    Add freeboard and auto generate config file for freeboard

--- routes/devices.js
+++ routes/devices.js
@@ -1,10 +1,13 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 var uuid = require('node-uuid');
 
 /* GET users listing. */
 router.get('/create', function(req, res, next) {
-  res.render('devices/create', {uuid: uuid.v4()});
+  let newDeviceId = uuid.v4();
+  res.render('devices/create', {uuid: newDeviceId});
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Hey', message: 'Hello there!'});
+  res.render('index', {title: 'ESP To Freeboard', message: 'Create new device now to control it with freeboard for free!'});
 });
 
 module.exports = router;
